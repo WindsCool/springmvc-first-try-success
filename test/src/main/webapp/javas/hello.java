c9a351fdd76efb1d16773f5485a5d5e83eab37dc
denglinfeng <denglinfeng@denglinfengdeMacBook-Pro.local>
2018-06-04 21:56:57 +0800

    first commit

---
+++ test/src/main/webapp/javas/hello.java
@@ -0,0 +1,13 @@
+package main.webapp.javas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class hello {
+    @RequestMapping(value = "/test.do",method = RequestMethod.GET)
+    public String test() {
+        return "hello";
+    }
+}
---
+++ test/target/test/WEB-INF/classes/main/webapp/WEB-INF/content/hello.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello邓琳枫</h2>
+</body>
+</html>
